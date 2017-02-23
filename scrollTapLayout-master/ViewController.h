e1877c2854c5be37f476fb30fc80fd982203d456
ZongAng123 <zongang15@yahoo.com>
2017-02-23 13:56:43 +0800

    Initial Commit

---
+++ scrollTapLayout-master/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scrollTapLayout-master
+//
+//  Created by 纵昂 on 2017/2/23.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ scrollTapLayout-master/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  scrollTapLayout-master
+//
+//  Created by 纵昂 on 2017/2/23.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
